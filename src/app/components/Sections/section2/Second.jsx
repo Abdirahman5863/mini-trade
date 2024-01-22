40f2da536538700b0771c9c9bf2ac9762874242f
Abdirahman5863 <abdirahmanabdi5863@gmail.com>
2024-01-22 11:20:48 +0300

    addtional

--- src/app/components/Sections/section2/Second.jsx
+++ src/app/components/Sections/section2/Second.jsx
@@ -7,9 +7,13 @@ const Second = () => {
       <div className="flex-1 relative p-[200px] w-[200px] h-[200px] object-fill items-center"></div>
       <div className="Aside">
         <div>
-          <h2 className="text-white pt-11">
-            <span>MiniTrade</span> is the ‘Web3 Amazon’ for growing your wealth.
-          </h2>
+          <p className="text-white text-[20px] font-thin p-[40px] w-[740px] ga">
+            <span className="text-xl bold text-[30px] font-extrabold">
+              MiniTrade
+            </span>
+            is the ‘Web3 Amazon’ <br />
+            for growing your wealth.
+          </p>
           <p className="text-white">
             Never miss savings opportunities, staking and even high-performin
             new crypto listings
@@ -21,6 +25,27 @@ const Second = () => {
             (gaming, betting & etc)
           </p>
           <button className="button-g">Learn More</button>
+        </div>
+        <div>
+          <p>
+            MiniTrade is the ‘Web3 Amazon’ for growing your wealth. Never miss
+            savings opportunities, staking and even high-performing new crypto
+            listings. Web 3 aggregator of the best performing assets globally
+            including crypto, stocks, managed portfolios, private companies
+            (startups), new listings, bonds, staking & savings, and even
+            predictions (gaming, betting & etc) ALL IN ONE WEB3 APP. Grow your
+            wealth , even if it is as low as USD 1 dollar Expert team of 18
+            research analysts full-time selecting the best opportunities. You
+            decide your risk threshold and filter money-growing assets that fit
+            your style. Kinda like, set-it and forget-it. Grow your wealth while
+            you sleep with alerts that keep you ahead and on top of your money
+            What’s in it for MiniTrade? We earn performance fees of 8.0% on
+            profits made within the platform. What do you need? Web3 Crypto
+            wallet (MetaMask, TrustWallet) topped-up with assets including:
+            USDC, USDT, ETH or BNB Invest as little as USD 1 Dollar (we are
+            radically serious about inclusiveness and democratization and all
+            humanity should have equal opportunities to grow their wealth.
+          </p>
         </div>
       </div>
     </div>
